56f8e15d81b268fb033b5b029e3815607561e9ad
julielaursen <julielaursen1@gmail.com>
2022-10-22 22:55:08 -0500

    update task text

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,9 +1,34 @@
 import Header from './components/Header'
+import Tasks from './components/Tasks'
+import {useState} from 'react'
 
 const App = () => {
+
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctor's appointment",
+      day: 'Nov 4, 2:30pm',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Send thank you notes',
+      day: 'Oct 23, 8:00pm',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Call mom',
+      day: 'Oct 23, 9:00pm',
+      reminder: true,
+    },
+  ]); 
+
   return (
     <div className='container'>
       <Header />
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
--- react-task-tracker/src/components/Header.js
+++ react-task-tracker/src/components/Header.js
@@ -1,25 +1,25 @@
 import PropTypes from 'prop-types'
+import Button from './Button'
 
 const Header = ({ title }) => {
+  const onClick = (e) => {
+    console.log(e)
+  }
+
   return (
-    <header>
-    <h1>{title}</h1>
+    <header className="header">
+      <h1>{title}</h1>
+      <Button color="steelblue" text="Add" onClick={onClick} />
     </header>
   )
 }
 
 Header.defaultProps = {
-    title: 'Task Tracker',
+  title: 'Task Tracker',
 }
 
 Header.propTypes = {
-    title: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
 }
-
-// CSS in JS
-// const headingStyle = {
-//     color: 'red', 
-//     backgroundColor: 'black'
-// }
 
 export default Header

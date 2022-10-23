d77a43ac40e00c22da134fba307006d56f8d7cea
julielaursen <julielaursen1@gmail.com>
2022-10-22 23:36:37 -0500

    start addtask

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,9 +1,9 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
-import {useState} from 'react'
+import AddTask from './components/AddTask'
+import { useState } from 'react'
 
 const App = () => {
-
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -23,14 +23,33 @@ const App = () => {
       day: 'Oct 23, 9:00pm',
       reminder: true,
     },
-  ]); 
+  ])
 
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task,
+      ),
+    )
+  }
+
+  // app.js is where we get access to state
+  // state gets passed down, actions get passed up
   return (
-    <div className='container'>
+    <div className="container">
       <Header />
-      <Tasks tasks={tasks}/>
+      <AddTask />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        'There are no tasks'
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
